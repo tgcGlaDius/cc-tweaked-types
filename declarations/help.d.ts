ca689e8e51ca021727bc35199f6401e85b098dcb
tgcGlaDius <eliaswa@icloud.com>
2021-01-25 22:56:48 +0100

    added help

---
+++ declarations/help.d.ts
@@ -0,0 +1,11 @@
+declare namespace help{
+    function path(): string;
+
+    function setPath(newPath: string): void;
+
+    function lookup(topic:string): string | null;
+
+    function topics(): string[];
+
+    function completeTopic(prefix:string): string[];
+}
