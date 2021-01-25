e52e67bcc5ab65b256c97f3b0dd87b256bbbf342
tgcGlaDius <eliaswa@icloud.com>
2021-01-25 22:35:13 +0100

    added disk declaration

---
+++ declarations/disk.d.ts
@@ -0,0 +1,23 @@
+declare namespace disk{
+    function isPresent(name:string): boolean;
+
+    function getLabel(name:string): string | null;
+
+    function setLabel(name:string, label:string): void;
+
+    function hasData(name:string): boolean;
+
+    function getMountPath(name:string): string | null;
+
+    function hasAudio(name:string): boolean;
+
+    function getAudioTitle(name:string): string | false | null;
+
+    function playAudio(name:string): void;
+
+    function stopAudio(name:string): void;
+
+    function eject(name:string): void;
+
+    function getID(name:string): string | null;
+}
