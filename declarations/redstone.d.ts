ef63748afc4543717769bbd44d8f9d7752e6a85c
tgcGlaDius <eliaswa@icloud.com>
2021-01-26 11:57:12 +0100

    added redstone

---
+++ declarations/redstone.d.ts
@@ -0,0 +1,25 @@
+declare namespace redstone{
+    type Side = "top" | "bottom" | "left" | "right" | "front" | "back";
+
+    function getSides(): Side[];
+
+    function setOutput(side: Side, on: boolean): void;
+
+    function getOutput(side: Side): boolean;
+
+    function getInput(side: Side): boolean;
+
+    function setAnalogOutput(side: Side, value: number): void;
+
+    function getAnalogOutput(side: Side): number;
+
+    function getAnalogInput(side: Side): number;
+
+    function setBundledOutput(side: Side, output: colors.ColorSet): void;
+
+    function getBundledOutput(side: Side): number;
+
+    function getBundledInput(side: Side): number;
+
+    function testBundledInput(side: Side, mask: number): boolean;
+}
