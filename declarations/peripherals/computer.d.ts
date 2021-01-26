9534d136ad83be358e4c25b13e487778df942c87
tgcGlaDius <eliaswa@icloud.com>
2021-01-26 13:28:53 +0100

    added computer peripheral

---
+++ declarations/peripherals/computer.d.ts
@@ -0,0 +1,13 @@
+declare class computerPeripheral{
+    turnOn(): void;
+
+    shutdown(): void;
+
+    reboot(): void;
+
+    getID(): number;
+
+    isOn(): boolean;
+
+    getLabel(): string | null;
+}
