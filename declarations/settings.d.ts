1c7f8b7eb5ea5f58213340d86bebc7bf8cdccdb1
tgcGlaDius <eliaswa@icloud.com>
2021-01-26 12:06:47 +0100

    added settings

---
+++ declarations/settings.d.ts
@@ -0,0 +1,23 @@
+declare namespace settings{
+    type settingDetails = {description?: string, default?: any, type?: string, value?: any};
+
+    function define(name:string, options?: settingDetails): void;
+
+    function undefine(name:string): void;
+
+    function set(name:string, value:any): void;
+
+    function get(name:string, defaultValue?:any): any;
+
+    function getDetails(name:string): settingDetails;
+
+    function unset(name:string): void;
+
+    function clear(): void;
+
+    function getNames(): string[];
+
+    function load(sPath?: string): boolean;
+
+    function save(sPath?: string): boolean;
+}
