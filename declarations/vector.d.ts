c55a2f2824f299f67b6546042e9334eec57d9376
tgcGlaDius <eliaswa@icloud.com>
2021-01-26 12:44:58 +0100

    added vector

---
+++ declarations/vector.d.ts
@@ -0,0 +1,33 @@
+interface VectorConstructor{
+    new: (this:void, x:number, y:number, z:number) => Vector;
+}
+
+declare class Vector{
+    x: number; 
+    y: number; 
+    z: number;
+
+    add(o: Vector): Vector;
+
+    sub(o: Vector): Vector;
+
+    mul(m: number): Vector;
+
+    div(m: number): Vector;
+
+    unm(): Vector;
+
+    dot(o: Vector): Vector;
+
+    cross(o: Vector): Vector;
+
+    length(): number;
+
+    normalize(): Vector;
+
+    round(tolerance?: number): Vector;
+
+    tostring(): string;
+}
+
+declare var vector: VectorConstructor;
