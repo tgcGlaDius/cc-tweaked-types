9a36c71c1a6acf11e41d4034f0f4de3bfde15bff
tgcGlaDius <eliaswa@icloud.com>
2021-01-26 12:53:01 +0100

    added window

---
+++ declarations/window.d.ts
@@ -0,0 +1,15 @@
+declare namespace window{
+    function create(parent: Redirect, nX: number, nY: number, nWidth: number, nHeight: number, bStartVisible?: boolean): Window;
+}
+
+declare class Window extends Redirect{
+    setVisible(visible: boolean): void;
+
+    redraw(): void;
+
+    restoreCursor(): void;
+
+    getPosition(): [x: number, y: number];
+
+    reposition(x: number, y: number, width?: number, height?: number): void;
+}
