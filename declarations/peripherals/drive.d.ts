a8f9aaab661a5dc17bc4e344b91936d390e6ef3d
tgcGlaDius <eliaswa@icloud.com>
2021-01-27 13:44:11 +0100

    added disk peripheral

---
+++ declarations/peripherals/drive.d.ts
@@ -0,0 +1,24 @@
+/** @noSelfInFile */
+declare class drivePeripheral{
+    isDiskPresent(): boolean;
+
+    getDiskLabel(): string | null;
+
+    setDiskLabel(label?: string): string | null;
+
+    hasData(): boolean;
+
+    getMountPath(): string | null;
+
+    hasAudio(): boolean;
+
+    getAudioTitle(): string | null | false;
+
+    playAudio(): void;
+
+    stopAudio(): void;
+
+    ejectDisk(): void;
+
+    getDiskID(): number | null;
+}
