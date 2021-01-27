f6501c6f47d7957200133256a0ff375670110b19
tgcGlaDius <eliaswa@icloud.com>
2021-01-27 13:57:54 +0100

    added monitor peripheral

---
+++ declarations/peripherals/monitor.d.ts
@@ -0,0 +1,40 @@
+/** @noSelfInFile */
+declare class monitorPeripheral extends Redirect{
+    setTextScale(scale:number): void;
+
+    getTextScale(): number;
+
+    write(text:string): void;
+
+    scroll(y: number): void;
+
+    getCursorPos(): [x: number, y: number];
+
+    setCursorPos( x: number, y: number): void;
+
+    getCursorBlink(): boolean;
+
+    setCursorBlink(blink:boolean): void;
+
+    getSize(): [width: number, height: number];
+
+    clear(): void;
+
+    clearLine(): void;
+
+    getTextColour(): colors.ColorSet;
+
+    setTextColour(colour:colors.ColorSet): void;
+
+    getBackgroundColour(): colors.ColorSet;
+
+    setBackgroundColour(colour:colors.ColorSet): void;
+
+    isColour(): boolean;
+
+    blit( text: string, textColour: string, backgroundColour: string): void;
+
+    setPaletteColour(index: number, colour: number): void;
+
+    getPaletteColour(colour: number): colors.UnpackedRGB;
+}
